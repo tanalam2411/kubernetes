a1348777e93f71c80cae93c4088bc9ac1b26807a
Tanveer Alam <tan24alam11@gmail.com>
2020-01-18 23:48:53 +0530

    k8s api convention update and TODO added.

---
+++ k8s_api/k8s_api_conventions/list_types.go
@@ -0,0 +1,36 @@
+package main
+//
+//import (
+//	"fmt"
+//	"go/importer"
+//	//podRest "k8s.io/kubernetes/pkg/registry/core/pod/rest"
+//	//	podStorage "k8s.io/kubernetes/pkg/registry/core/pod/storage"
+//)
+//
+//func main() {
+//	ret := importer.Lookup("k8s.io/kubernetes/pkg/registry/core/pod/rest")
+//	if ret.err != nil {
+//		fmt.Printf("error: %s\n", err.Error())
+//		return
+//	}
+//
+//
+//	for _, declName := range pkg.Scope().Names() {
+//		fmt.Println(declName)
+//	}
+//}
+
+/*
+
+type AttachREST struct
+type ExecREST struct
+type PortForwardREST struct
+type ProxyREST struct
+
+type EvictionREST struct
+
+type podEphemeralContainersStrategy struct
+type podStatusStrategy struct
+type podStrategy struct
+type podStrategyWithoutGraceful struct
+ */
